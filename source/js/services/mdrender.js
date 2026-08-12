d6bbb06db820650afe6ddc9e07d40408ea5172d1
watermelonabc <2172718424@qq.com>
2026-08-12 13:16:46 +0800

    Add 'DOMPurify' as global to avoid ESLint error

--- source/js/services/mdrender.js
+++ source/js/services/mdrender.js
@@ -1,9 +1,10 @@
+/* global DOMPurify */
 (function () {
   const els = document.getElementsByClassName('ds-mdrender');
   for (var i = 0; i < els.length; i++) {
     const el = els[i];
     const src = `${el.getAttribute('src')}?t=${new Date().getTime()}`;
-    
+
     utils.request(el, src, async resp => {
       const data = await resp.text();
       if (typeof DOMPurify !== 'undefined') {
