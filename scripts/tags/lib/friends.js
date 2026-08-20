9e11030478ce266298b3425fd27ee5585da369be
watermelonabc <2172718424@qq.com>
2026-08-20 15:30:07 +0800

    Add general alt for images

--- scripts/tags/lib/friends.js
+++ scripts/tags/lib/friends.js
@@ -28,7 +28,7 @@ module.exports = ctx => function(args) {
         el += `<div class="grid-cell user-card">`
         el += `<a class="card-link" target="_blank" rel="external nofollow noopener noreferrer" href="${item.url}">`
         el += `<div class="lazy-box icon">`
-        el += `<img class="lazy" data-src="${item.icon || item.avatar || ctx.theme.config.default.avatar}" onerror="javascript:this.removeAttribute(&quot;data-src&quot;);this.src=&quot;${ctx.theme.config.default.avatar}&quot;;"/>`
+        el += `<img class="lazy" alt="Friend's avatar" data-src="${item.icon || item.avatar || ctx.theme.config.default.avatar}" onerror="javascript:this.removeAttribute(&quot;data-src&quot;);this.src=&quot;${ctx.theme.config.default.avatar}&quot;;"/>`
         el += `<div class="lazy-icon"></div>`
         el += `</div>`
         el += `<div class="name">`
